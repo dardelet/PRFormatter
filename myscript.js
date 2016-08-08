fb9fd0696fe4b3f067b81bf2647fc81efcf8b6f4
Guillaume Dardelet <guillaume.dardelet@telecom-paristech.fr>
2016-08-08 23:17:55 +0200

    First API call

--- myscript.js
+++ myscript.js
@@ -1,24 +1,14 @@
-//alert('it works');
-/*api_key = "dc6zaTOxFJmzC";
-base_url = "https://api.giphy.com";
-path = "/v1/gifs/search"
-q = "thumbs+up";
-limit = 10;
+var api_key = 'b3a641fc626ace184d2a4d76e9d2aa54';
 
-
-
-template = `<button type="button" id = "add-gif" class="js-toolbar-item toolbar-item tooltipped tooltipped-n" aria-label="Insert a GIF" tabindex="-1">
-          <svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>
-        </button>`;
-
-$('.comment-form-head .toolbar-group').eq(1).append(template);
-$('#add-gif').click(function(){
-    $.get(
-        base_url+path+"?q="+q+"&limit="+limit+"&api_key="+api_key,
-        function (data){
-            var lol = JSON.stringify(data.data[0].images.fixed_height.url);
-            alert(lol);
-        }
-    );
-})*/
-alert('lol');
+var authenticationSuccess = function() { console.log("Successful authentication"); };
+var authenticationFailure = function() { console.log("Failed authentication"); };
+Trello.authorize({
+  type: "popup",
+  name: "PR Formatter",
+  scope: {
+    read: true,
+    write: true },
+  expiration: "never",
+  success: authenticationSuccess,
+  error: authenticationFailure
+});

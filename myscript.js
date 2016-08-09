c73d48cb71b0642a6481dc1cd54c35b5d168c87c
Guillaume Dardelet <guillaume.dardelet@telecom-paristech.fr>
2016-08-09 17:05:04 +0200

    Reorganizing

--- myscript.js
+++ myscript.js
@@ -1,18 +1,6 @@
 var featureTag = $('i:contains("compare")').next()
 var cardId = parseInt(featureTag.html().match(/\d+/));
 
-function formatPrWithCard(card) {
-  var strippedCardTitle = card['name']
-    .replace(/\s?\(\d+\)\s?/, '')
-    .replace(/\s?\[\d+\]\s?/, '');
-  var prTitle = '[' + card['idShort'] + '] ' + strippedCardTitle;
-  $('#pull_request_title').val(prTitle);
-
-  var prTemplate = $('#pull_request_body').val()
-    .replace(/## COPIER\/COLLER la description de la User Story ici !/, '')
-  $('#pull_request_body').html(card['desc'] + prTemplate);
-}
-
 Trello.authorize({
   type: "popup",
   name: "PR Formatter",
@@ -30,14 +18,21 @@ Trello.authorize({
           if (response[i]['name'] === 'Doing')
             break;
         Trello.get('/lists/' + response[i]['id'] + '/cards', function (response) {
-          for(var i = 0 ; i < response.length ; i++)
-            if (response[i]['idShort'] === cardId)
-              formatPrWithCard(response[i])
+          for(var i = 0 ; i < response.length ; i++) {
+            if (response[i]['idShort'] === cardId) {
+              var card = response[i];
+              var strippedCardTitle = card['name']
+                .replace(/\s?\(\d+\)\s?/, '')
+                .replace(/\s?\[\d+\]\s?/, '');
+              var prTitle = '[' + card['idShort'] + '] ' + strippedCardTitle;
+              $('#pull_request_title').val(prTitle);
+              var prTemplate = $('#pull_request_body').val()
+                .replace(/## COPIER\/COLLER la description de la User Story ici !/, '')
+              $('#pull_request_body').html(card['desc'] + prTemplate);
+            }
+          }
         });
       });
     });
-  },
-  error: function() {
-    console.log("Failed authentication");
   }
 });
